419971794a07a5dbcbae0a1a97af3bf466e43804
kharkovska-iryna <i.kharkovskaya@ua.fm>
2022-07-12 22:17:20 +0300

    changed file main.js

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ let firstLetterUserSurname = userSurname[0].toUpperCase();
 let fullUserSurnameWithoutFirstLetter = userSurname.slice(1, userSurname.length);
 let fullUserName = `${firstLetterUserName}${fullUserNameWithoutFirstLetter}`;
 let fullUserSurname = `${firstLetterUserSurname}${fullUserSurnameWithoutFirstLetter}`;
-let fullName = fullUserName + ' ' + fullUserSurname;
+let fullName = `${fullUserName} ${fullUserSurname}`;
 console.log('Full name:' + `${fullName}`);
 
 // user email
 
-let userEmail = prompt('Enter your e-mail', '').trim();
+let userEmail = prompt('Enter your e-mail', '');
 let email = userEmail.toLowerCase();
+email = email.replaceAll(' ', '').trim();
 if (email.includes('@')) {
     if (email.startsWith('@')) {
         email = 'not valid email' + ' ' + email + ' ' + '(symbol @ find in first place)';   
@@ -23,7 +24,7 @@ if (email.includes('@')) {
         email = 'not valid email' + ' ' + email + ' ' + '(symbol @ find in last place';
         // console.log('not valid email' + ' ' + email + ' ' + '(symbol @ find in last place)');
     } else {
-        email = 'Email:' + ' ' + email;
+        email = email;
         // console.log('Email:' + ' ' + email);
     }
 }
@@ -33,7 +34,8 @@ else {
 }
 
 // user age
-let yearOfBirth = +prompt('Enter your year of birth', '').trim();
+let yearOfBirth = prompt('Enter your year of birth', '');
+yearOfBirth = +(yearOfBirth.replaceAll(' ', '').trim());
 let currentYear = new Date().getFullYear();
 let age = currentYear - yearOfBirth;
 console.log(age);
